aece586f0f73d7dbfcf23ac134f7f422cf5ca453
Ola Ipaye <ipayeola@outlook.com>
2024-04-27 10:44:22 +0100

    created new file: basic_operations and added basic python arithmetic operations to it.

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+# Basic arithmetic operations in Python
+a = 3
+b = 5
+c = 8
+
+print(a + b)
+
+# Using the type() fucntion to find out the type of data type is stored in that variable.
+print(type(a)) # used the type function insdie the print command to output the code.
+
