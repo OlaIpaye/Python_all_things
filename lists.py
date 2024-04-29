e031b09437c5ccf436e14904158c748a78703fb4
Ola Ipaye <ipayeola@outlook.com>
2024-04-30 00:01:41 +0100

    Updated list.py file with slicing and dicing method in python.

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
 # A list is used to store multiple values in Python
 
 # Using a list to store names of my friends
-friends = ["Dave", "Peter", "Dan", "Joe"]
+friends = ["Dave", "Peter", "Dan", "Joe", "moh", "femi"]
 print(friends)
 
 # Print out the type of friends - what data type is the friends list?
@@ -9,3 +9,11 @@ print(type(friends))
 
 # Print out the friend, with the name starting with J
 print(friends[3])
+
+# Print out the first 2 element in the list using slicing and dicing method and store in new variable
+slicing_names = friends[:2]
+print(slicing_names)
+
+# Print out the last 4 element in the list using slicing and dicing method and store in new variable
+dicing_names = friends[-4:]
+print(dicing_names)
