bf4a6072a3775d0671d9baba67e7a3b16f429e29
Ola Ipaye <ipayeola@outlook.com>
2024-04-29 23:35:13 +0100

    Updated list.py with how to subset from a list.

--- lists.py
+++ lists.py
@@ -3,3 +3,9 @@
 # Using a list to store names of my friends
 friends = ["Dave", "Peter", "Dan", "Joe"]
 print(friends)
+
+# Print out the type of friends - what data type is the friends list?
+print(type(friends))
+
+# Print out the friend, with the name starting with J
+print(friends[3])
