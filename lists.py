81e8f7cae74d77a24666123efc777075a9845639
Ola Ipaye <ipayeola@outlook.com>
2024-05-02 23:15:53 +0100

    Updated list.py file with some list methods such as list() and copy().

--- lists.py
+++ lists.py
@@ -1,19 +1,37 @@
 # A list is used to store multiple values in Python
 
-# Using a list to store names of my friends
-friends = ["Dave", "Peter", "Dan", "Joe", "moh", "femi"]
-print(friends)
+# # Using a list to store names of my friends
+# friends = ["Dave", "Peter", "Dan", "Joe", "moh", "femi"]
+# print(friends)
 
-# Print out the type of friends - what data type is the friends list?
-print(type(friends))
+# # Print out the type of friends - what data type is the friends list?
+# print(type(friends))
 
-# Print out the friend, with the name starting with J
-print(friends[3])
+# # Print out the friend, with the name starting with J
+# print(friends[3])
 
-# Print out the first 2 element in the list using slicing and dicing method and store in new variable
-slicing_names = friends[:2]
-print(slicing_names)
+# # Print out the first 2 element in the list using slicing and dicing method and store in new variable
+# slicing_names = friends[:2]
+# print(slicing_names)
 
-# Print out the last 4 element in the list using slicing and dicing method and store in new variable
-dicing_names = friends[-4:]
-print(dicing_names)
+# # Print out the last 4 element in the list using slicing and dicing method and store in new variable
+# dicing_names = friends[-4:]
+# print(dicing_names)
+
+#END
+
+# A list of ages
+people_ages = [40, 10, 11.5, 28, 34, 69]
+
+#Create a copy of the list above using list constructor and save in new variable
+copy_people_ages = list(people_ages)
+
+#Create a copy of the list above using the copy method and save in new variable
+copy_people_ages1 = people_ages.copy()
+
+# Reassign an element in the original list, without changing the original people_ages list
+copy_people_ages[1] = 20
+
+print(people_ages)
+print(copy_people_ages) # Replaces the 10 with 20.
+print(copy_people_ages1)
