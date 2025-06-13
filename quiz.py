0bbcedfde3f4fbf884ab892b023ceef9a66b88fd
Ola Ipaye <ipayeola@outlook.com>
2025-06-13 21:45:51 +0100

    New quiz game, learning from Tech with Tim on YT.

---
+++ quiz.py
@@ -0,0 +1,11 @@
+# welcome message
+print("Welcome to my Quiz!")
+
+# Ask user if they want to play
+playing = input("Do you want to play my game? ")
+
+if playing != "yes":
+    print("Okay, maybe next time!")
+    quit()
+
+print("Great! Let's get started :)")
