a164f8ffce59b01df5454315fef704e1b5fc5573
Ola Ipaye <ipayeola@outlook.com>
2025-06-13 22:48:45 +0100

    Updated quiz game.

--- quiz.py
+++ quiz.py
@@ -10,18 +10,23 @@ if playing != "yes":
 
 print("Great! Let's get started :)")
 
-# ask user another question
+#initialize score to keep track of correct answers
+score = 0
+
+# ask user multiple questions
 answer = input("What is the capital of France? ")
 
 if answer.lower() == "paris":
     print("Correct!")
+    score += 1
 else:
-    print("Incorrect! The correct answer is paris.")
+    print("Incorrect! The correct answer is Paris.")
 
 answer = input("What is the Capital of Nigeria? ")
 
 if answer.lower() == "abuja":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect! The correct answer is abuja.")
 
@@ -29,6 +34,7 @@ answer = input("What does CPU stands for? ")
 
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect! The correct answer is central processing unit.")
 
@@ -36,6 +42,12 @@ answer = int(input("How many cities are in England? "))
 
 if answer == 75:
     print("Correct!")
+    score += 1
 else:
     print("Incorrect! The correct answer is 75.")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
+print("Thanks for playing my game!")
+# End of the quiz
 
