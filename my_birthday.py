b0616a4469b1f9afb6860b6a4d7b7091596839c0
Ola Ipaye <ipayeola@outlook.com>
2025-08-04 14:23:05 +0100

    updated the birthday file to output the day, week day of my birthday using different functions from the datetime module.

--- my_birthday.py
+++ my_birthday.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
 # the datetime object keeps track of my birthday
-birthday = datetime(1994, 2, 8)
+birthday = datetime(1994, 2, 8, 6, 35, 12)
 
 print(birthday)
+
+print(birthday.year) # getting the year of my birthday
+
+print(birthday.weekday()) # getting the week day of my birthday
+
+print(birthday.now()) # getting the date for the present time
 
 
