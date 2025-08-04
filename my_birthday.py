dc9907187ddcf4ee2e06b1eaba6eb19626631886
Ola Ipaye <ipayeola@outlook.com>
2025-08-04 14:44:50 +0100

    updated the birthday file by leanring how to use the strptime function to take a string, parse through it and convert it into a date time.

--- my_birthday.py
+++ my_birthday.py
@@ -11,4 +11,10 @@ print(birthday.weekday()) # getting the week day of my birthday
 
 print(birthday.now()) # getting the date for the present time
 
+print(f"It's been {datetime.now() - datetime(2025, 2, 8)} since my birthday!") # using datetime module to get time difference
+
+parse_date = datetime.strptime("Feb 8, 2025", "%b %d, %Y")
+print(parse_date.year)
+print(parse_date.month)
+print(parse_date.day)
 
